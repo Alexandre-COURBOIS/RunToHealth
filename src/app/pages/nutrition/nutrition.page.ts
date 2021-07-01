8b1b2db9744be3a623ed28834b63a4e7f0152e71
Pierre <pierre@halavent@gmail.com>
2021-07-01 20:12:35 +0200

    displaying meals and their calories, putting api call in service

--- src/app/pages/nutrition/nutrition.page.ts
+++ src/app/pages/nutrition/nutrition.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {environment} from '../../../environments/environment';
 import { DejeunerModalPage } from "../../modals/dejeuner-modal/dejeuner-modal.page";
 import {ModalController} from "@ionic/angular";
+import {NutritionService} from "../../services/nutrition.service";
+import {Storage} from "@ionic/storage-angular";
+import {PetitdejeunerModalPage} from "../../modals/petitdejeuner-modal/petitdejeuner-modal.page";
+import {DinerModalPage} from "../../modals/diner-modal/diner-modal.page";
 
 @Component({
   selector: 'app-nutrition',
@@ -10,30 +13,96 @@ import {ModalController} from "@ionic/angular";
   styleUrls: ['./nutrition.page.scss'],
 })
 export class NutritionPage implements OnInit {
-  test: any;
+  petitDejCal: any;
+  dejCal: any;
+  dinerCal: any;
 
-  constructor(private httpClient: HttpClient, private modalController: ModalController) { }
-
+  constructor(private httpClient: HttpClient, private modalController: ModalController, public nutritionService: NutritionService, private storage: Storage) { }
   ngOnInit() {
-    this.test = this.httpClient.post("https://trackapi.nutritionix.com/v2/natural/nutrients", {
-      query: "eggs",
-    },
-      {
-        headers: {
-          'x-app-id': environment.NUTRITIONIX_APP_ID,
-          'x-app-key': environment.NUTRITIONIX_KEYS_ID
-        }
-      }).subscribe(value => {
-        console.log(value)
-    })
+    this.storage.create();
 
+    this.updatePetitDejCal();
+    this.updateDejCal();
+    this.updateDinerCal();
   }
 
-  async presentModal() {
+  async presentDejeunerModal() {
     const modal = await this.modalController.create({
       component: DejeunerModalPage
     });
+
+    modal.onDidDismiss().then(value => {
+      if(value) {
+        this.updateDejCal();
+      }
+    });
+
     return await modal.present();
+  }
+
+  async presentPetitDejeunerModal() {
+    const modal = await this.modalController.create({
+      component: PetitdejeunerModalPage
+    });
+
+    modal.onDidDismiss().then(value => {
+      if(value) {
+        this.updatePetitDejCal();
+      }
+    });
+
+    return await modal.present();
+  }
+
+  async presentDinerModal() {
+    const modal = await this.modalController.create({
+      component: DinerModalPage
+    });
+
+    modal.onDidDismiss().then(value => {
+      if(value) {
+        this.updateDinerCal();
+      }
+    });
+
+    return await modal.present();
+  }
+
+  updateDejCal() {
+    // @ts-ignore
+    this.storage.get('dejeuner').then(value => {
+      if(value) {
+        console.log(value);
+        this.dejCal = value;
+      } else {
+        console.log("STORAGE VIDE")
+        this.dejCal = "";
+      }
+    })
+  }
+  updatePetitDejCal() {
+    // @ts-ignore
+    this.storage.get('petitdejeuner').then(value => {
+      if(value) {
+        console.log(value);
+        this.petitDejCal = value;
+      } else {
+        console.log("STORAGE VIDE")
+        this.petitDejCal = "";
+      }
+    })
+  }
+  updateDinerCal() {
+    // @ts-ignore
+    this.storage.get('diner').then(value => {
+      if(value) {
+        console.log(value);
+        this.dinerCal = value;
+      } else {
+        console.log("STORAGE VIDE")
+        this.dinerCal = "";
+      }
+    })
   }
 
 }
